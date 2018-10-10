3dd9a4ea1ef8f346784b0f43f3a7107ffe68bce4
liuyu <306487103@qq.com>
2018-10-10 13:52:06 +0800

    完成基础配置

--- ib-web/src/main/java/com/ez/ib/web/IbWebApplication.java
+++ ib-web/src/main/java/com/ez/ib/web/IbWebApplication.java
@@ -1,9 +1,13 @@
 package com.ez.ib.web;
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MybatisAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.ez.**"})
 public class IbWebApplication {
 
     public static void main(String[] args) {
