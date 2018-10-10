0c8f367513f85d697ce97357911efb24ae1aa17f
liuyu <306487103@qq.com>
2018-10-10 11:28:36 +0800

    初始化

---
+++ ib-web/src/main/java/com/ez/ib/web/IbWebApplication.java
@@ -0,0 +1,12 @@
+package com.ez.ib.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbWebApplication.class, args);
+    }
+}
